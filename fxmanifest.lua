208e7c3505fc714d440cbe6fb0f3660e111149c4
unknown <arsenic.contac@gmail.com>
2024-02-01 14:26:28 +0100

    Correction #2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,13 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-author 'Arsenic'
+author 'Arsenicooo'
 description 'Loading Screen'
 version '1.0.0'
 
 loadscreen 'index.html'
 files {
     'index.html',
-    'src/index.css',
-    'src/background.png'
+    'index.css',
+    'background.png'
 }
