86928b12d2b679f8a141365b445af657cb4d0adf
unknown <arsenic.contac@gmail.com>
2024-02-01 14:22:00 +0100

    Ajout des fichiers

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Arsenic'
+description 'Loading Screen'
+version '1.0.0'
+
+loadscreen 'index.html'
+files {
+    'index.html',
+    'src/index.css',
+    'src/background.png'
+}
